aa6a02c80c240df11944f19b7ff8171199645824
mortimerxxxx <66492764+mortimerxxxx@users.noreply.github.com>
2020-06-05 22:37:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.post-button.share').click(function(event) {
+        $('.share-window').toggleClass('active');
+    });
+});
+$(document).ready(function() {
+    $('.m-menu').click(function(event) {
+        $('.menu-sidebar').toggleClass('active');
+    });
+});
